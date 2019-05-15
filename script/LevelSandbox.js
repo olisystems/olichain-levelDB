ddedf1a2db2b20ed7d49ed0f39f584ab623de55c
m-yahya <yahya_gis@hotmail.com>
2019-05-15 14:12:02 +0200

    Config: levelDB

---
+++ script/LevelSandbox.js
@@ -0,0 +1,52 @@
+const level = require('level');
+const chainDB = '../chaindata';
+
+class LevelSandbox {
+    constructor() {
+        this.db = level(chainDB);
+    }
+    // add data to levelDB
+    addLevelDBData(key, value) {
+        return new Promise((resolve, reject) => {
+            this.db.put(key, value, error => {
+                if (error) {
+                    console.log(error);
+                    reject(error);
+                } else {
+                    console.log('Block ' + key + ' is saved');
+                    resolve();
+                }
+            })
+        })
+    }
+    // get data from levelDB
+    getLevelDBData(key) {
+        return new Promise((resolve, reject) => {
+            this.db.get(key, (error, value) => {
+                if (error) {
+                    console.log('Block ' + key + ' failed to retrieve', error);
+                    reject(error);
+                } else {
+                    resolve(value);
+                }
+            })
+        })
+    }
+
+    // get data count
+    getBlocksCount() {
+        return new Promise((resolve, reject) => {
+            let count = 0;
+            this.db.createReadStream().on('data', data => {
+                count++;
+            }).on('error', error => {
+                console.log(error);
+                reject(error);
+            }).on('close', () => {
+                resolve(count);
+            })
+        })
+    }
+}
+
+module.exports.LevelSandbox = LevelSandbox;
