3d6ec3ea3383e2b5ddd2e08a38f1c74ca788ac00
m-yahya <yahya_gis@hotmail.com>
2019-05-15 14:50:05 +0200

    Create Block Data Model

---
+++ script/Block.js
@@ -0,0 +1,27 @@
+class Block {
+    constructor() {
+        this.author = '';
+        this.difficulty = '';
+        this.extraData = '';
+        this.extraData = '';
+        this.gasLimit = '';
+        this.gasUsed = '';
+        this.hash = '';
+        this.logsBloom = '';
+        this.miner = '';
+        this.number = '';
+        this.parentHash = '';
+        this.receiptsRoot = '';
+        this.sealFields = [];
+        this.sha3Uncles = '';
+        this.signature = '';
+        this.size = '';
+        this.stateRoot = '';
+        this.step = '';
+        this.timestamp = '';
+        this.totalDifficulty = '';
+        this.transactions = []
+    }
+}
+
+module.exports.Block = Block;
