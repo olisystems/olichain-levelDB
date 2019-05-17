84527508e30800e34143aaa6f7568660fe6adc4b
m-yahya <yahya_gis@hotmail.com>
2019-05-17 19:34:33 +0200

    Remove Extra Functions

--- index.js
+++ index.js
@@ -1,47 +1,10 @@
-let blockchain = new Blockchain();
-//web3.eth.getBlockNumber().then(console.log);
+const Blockchain = require('./script/Blockchain.js');
+let blockchain = new Blockchain.Blockchain();
 
-// blockchain.getBlock(391175).then(console.log);
-
-
+// add block method
 blockchain.addBlock();
 
-// let totalBlocks = 0;
-// blockchain.db.getBlocksCount().then(count => {
-//     totalBlocks = count;
-//     console.log('Total blocks are: ' + totalBlocks);
-// });
-
-
-// for (var i = 0; i < 11; i++) {
-//     blockchain.getBlock(i).then(obj => {
-//         console.log(timeConverter.timeConverter(obj.timestamp));
-//     })
-// }
-
-//blockchain.db.getBlocksCount().then(console.log);
-
-function test() {
-    let fullBlocks = 0;
-    let emptyBlocks = 0;
-    for (var i = 0; i < 292746; i++) {
-        blockchain.getBlock(i).then(obj => {
-
-            if (obj.transactions.length != 0) {
-                fullBlocks++;
-                console.log('Full blocks are: ' + fullBlocks);
-                // obj.transactions.forEach(tx => {
-                //     console.log(timeConverter.timeConverter(obj.timestamp), tx.from)
-                // });
-                console.log(obj.transactions)
-            } else {
-                emptyBlocks++;
-                //console.log('Empty blocks are: ' + emptyBlocks);
-            }
-        });
-
-    }
-}
-//test();
+// validate chain
+// blockchain.validateChain().then(console.log);
 
 
