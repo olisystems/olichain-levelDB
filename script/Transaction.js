9c8f84478a3df52c0bc0c17a0619043d6f76ea5a
m-yahya <yahya_gis@hotmail.com>
2019-05-21 18:19:09 +0200

    Add Transaction Object

---
+++ script/Transaction.js
@@ -0,0 +1,20 @@
+class Transaction {
+    constructor() {
+        this.blockHash = '';
+        this.blockNumber = '';
+        this.chainId = '';
+        this.from = '';
+        this.gas = '';
+        this.gasPrice = '';
+        this.hash = '';
+        this.input = '';
+        this.nonce = '';
+        this.publicKey = '';
+        this.to = '';
+        this.transactionIndex = '';
+        this.value = '';
+        this.timestamp = '';
+    }
+}
+
+module.exports = Transaction;
